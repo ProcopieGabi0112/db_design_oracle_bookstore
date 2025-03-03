e950f308a94f1ba4d4251e3178b3cd09c93dd940
Procopie Gabriel <36031339+ProcopieGabi0112@users.noreply.github.com>
2025-03-03 23:25:19 +0200

    update

--- 3. database_steps/4. dpl_course/3. helpfull_scripts/3. checking_cdb_status.sql
+++
@@ -1,6 +0,0 @@
-ALTER SESSION SET CONTAINER = CDB$ROOT;
-SELECT name db_name,
-             open_mode state,
-             status db_status,
-             cdb final_status 
-FROM v$database,v$instance;
--- 3. database_steps/4. dpl_course/3. helpfull_scripts/6. checking_created_tablespaces.sql
+++
@@ -1,9 +0,0 @@
---setare conexiune;
-ALTER SESSION SET CONTAINER = CDB$ROOT;
-ALTER SESSION SET CONTAINER = bookstore_db;
-
-SELECT ts.tablespace_name, df.file_name,bytes/1000000000.0 AS gb_memory
-FROM dba_tablespaces ts
-JOIN dba_data_files df
-  ON ts.tablespace_name = df.tablespace_name;
---WHERE ts.tablespace_name LIKE 'BOOKSTORE_TABLESPACE';
